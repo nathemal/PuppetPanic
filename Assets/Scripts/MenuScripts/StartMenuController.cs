fbe3cac4a5d7650065edd32cd0059306207c77e8
nathemal <martin.carlsten.4152@student.uu.se>
2025-04-15 23:50:33 +0200

    Start Menu Script
    remade a script I already had and improved upon it. It has some placeholders that need to be replaced once the control and credits screen has been made

---
+++ Assets/Scripts/MenuScripts/StartMenuController.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class StartMenuController : MonoBehaviour
+{
+    Button startButton;
+    Button controlsButton;
+    Button creditsButton;
+
+    Button[] allButtons = new Button[3];
+
+    private void Awake()
+    {
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+        startButton = root.Q<Button>("StartButton");
+        controlsButton = root.Q<Button>("ControlsButton");
+        creditsButton = root.Q<Button>("CreditsButton");
+
+        allButtons[0] = startButton;
+        allButtons[1] = controlsButton;
+        allButtons[2] = creditsButton;
+    }
+
+    private void OnEnable()
+    {
+        ButtonActionsSubscribe();
+
+        startButton.Focus();
+    }
+
+    private void OnDisable()
+    {
+        ButtonActionsUnsubscribe();
+    }
+
+    private void ButtonActionsSubscribe()
+    {
+        startButton.clicked += OnStartButtonClicked;
+        controlsButton.clicked += OnControlsButtonClicked;
+        creditsButton.clicked += OnCreditsButtonClicked;
+
+        foreach (Button button in allButtons)
+        {
+            button.clicked += OnAllButtonsClicked;
+        }
+    }
+
+    private void ButtonActionsUnsubscribe()
+    {
+        startButton.clicked -= OnStartButtonClicked;
+        controlsButton.clicked -= OnControlsButtonClicked;
+        creditsButton.clicked -= OnCreditsButtonClicked;
+
+        foreach (Button button in allButtons)
+        {
+            button.clicked -= OnAllButtonsClicked;
+        }
+    }
+
+    // The code bellow this comment is mostly placeholders that will be replaced with the actual functions of the buttons later
+    private void OnStartButtonClicked() => ButtonClickedEvent("Start");
+    private void OnControlsButtonClicked() => ButtonClickedEvent("Controls");
+    private void OnCreditsButtonClicked() => ButtonClickedEvent("Credits");
+
+    private void OnAllButtonsClicked() => ButtonClickedEvent("A"); // This runs when any button is clicked so we can add sounds and other stuff that should happen at any button click
+
+    private void ButtonClickedEvent(string buttonType)
+    {
+        Debug.Log(buttonType + " Button was clicked");
+    }
+}
