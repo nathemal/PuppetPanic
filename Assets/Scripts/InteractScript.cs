3001bd8668d3153b98837e44876307ec5b22ce1b
SamStampler <lina.giesinger.0176@student.uu.se>
2025-04-29 15:20:50 +0200

    Pushable/Pullable Object
    Added pull out mechanic for drawer

--- Assets/Scripts/InteractScript.cs
+++ Assets/Scripts/InteractScript.cs
@@ -1,21 +1,37 @@
-using TMPro;
 using UnityEngine;
 
 public class InteractScript : MonoBehaviour
 {
     public GameObject pressE;
+    BoxCollider2D col;
+    bool canMove = false;
+    private void Start()
+    {
+        col = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && canMove)
+        {
+            col.enabled = true;
+            pressE.SetActive(false);
+        }
+    }
+
 
     public void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.tag == "Object")
+    {   
+        if(collision.tag == "Player" && !canMove)
         {
             pressE.SetActive(true);
+            canMove = true;
         }
     }
 
     public void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.tag == "Object")
+        if(collision.tag == "Player")
         {
             pressE.SetActive(false);
         }
