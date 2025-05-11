08da0bd8cca9ce6e4c7850f43decc8326e60ef3f
Marcel-Schiller <marcel.schiller.0806@student.uu.se>
2025-05-11 12:59:11 +0200

    win condition
    win condition script now triggers the win screen when 1 item is collected

--- Assets/Scripts/WinCondition.cs
+++ Assets/Scripts/WinCondition.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class WinCondition : MonoBehaviour
 {
     public int objWinAmount = 1;
+    public GameObject WinScreen;
 
     private void Update()
     {
@@ -14,7 +15,7 @@ public class WinCondition : MonoBehaviour
     {
         if(MainManager.objectCounter == objWinAmount)
         {
-            SceneManager.LoadScene("WinLoseScreensTest");
+            WinScreen.SetActive(true);
         }
     }
 }
