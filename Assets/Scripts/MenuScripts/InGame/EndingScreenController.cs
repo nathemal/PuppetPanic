ae8c3f98b45da2cba5539f0140fa208924797318
Marcel-Schiller <marcel.schiller.0806@student.uu.se>
2025-05-23 08:35:20 +0200

    Menu Scene Connection
    Replaced placeholder scene names in menu scripts with actual scene names to make loading the scenes possible.

--- Assets/Scripts/MenuScripts/InGame/EndingScreenController.cs
+++ Assets/Scripts/MenuScripts/InGame/EndingScreenController.cs
@@ -11,7 +11,7 @@ public class EndingScreenController : MonoBehaviour
     public MenuEvents menuEvents;
 
     private string mainMenu = "MainMenu";
-    private string startGame = "SampleScene"; // TODO: Replace SampleScene with the name of the first game scene
+    private string startGame = "Room 1"; // TODO: Replace SampleScene with the name of the first game scene
 
     private void OnEnable()
     {
