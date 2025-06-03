58b611afd97bfbb59b705810d99b7e81ddf74dfc
SamStampler <lina.giesinger.0176@student.uu.se>
2025-06-03 16:29:57 +0200

    Script for inventory UI

--- Assets/Scripts/InventoryUI.cs
+++ Assets/Scripts/InventoryUI.cs
@@ -3,29 +3,42 @@ using UnityEngine;
 
 public class InventoryUI : MonoBehaviour
 {
-    public GameObject featherUI;
-    public GameObject flowerUI;
-    public GameObject furUI;
-    public GameObject gemUI;
-    public GameObject ringUI;
+    public static GameObject featherUI;
+    public static GameObject flowerUI;
+    public static GameObject furUI;
+    public static GameObject gemUI;
+    public static GameObject ringUI;
 
-    public GameObject feather;
-    public GameObject flower;
-    public GameObject fur;
-    public GameObject gem;
-    public GameObject ring;
-
-
-    private void Update()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        IsCollected();
-    }
-
-    public void IsCollected()
-    {
-        if(ring.IsDestroyed())
+        if(collision.gameObject.name.Equals("Ring") && Input.GetKey(KeyCode.E))
         {
+            Debug.Log("ISCOLLECTED");
             ringUI.SetActive(true);
+        }
+
+        if (collision.gameObject.name.Equals("Object") && Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("ISCOLLECTED");
+            featherUI.SetActive(true);
+        }
+
+        if (collision.gameObject.name.Equals("Flower") && Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("ISCOLLECTED");
+            flowerUI.SetActive(true);
+        }
+
+        if (collision.gameObject.name.Equals("Fur") && Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("ISCOLLECTED");
+            furUI.SetActive(true);
+        }
+
+        if (collision.gameObject.name.Equals("Gem") && Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("ISCOLLECTED");
+            gemUI.SetActive(true);
         }
     }
 }
